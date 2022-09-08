537a20fe057117e9484b15cc167c35fc3b9b5e03
Jonathan Gainsley <38672162+gainsley@users.noreply.github.com>
2022-09-07 17:18:25 -0700

    add missing noreply file

---
+++ pkg/cloudcommon/noreply.go
@@ -0,0 +1,27 @@
+package cloudcommon
+
+import (
+	"github.com/edgexr/edge-cloud-platform/pkg/vault"
+)
+
+type EmailAccount struct {
+	Email    string `json:"email"`
+	User     string `json:"user"`
+	Pass     string `json:"pass"`
+	Smtp     string `json:"smtp"`
+	SmtpPort string `json:"smtpport"`
+	SmtpTLS  bool   `json:"smtptls"`
+}
+
+func GetNoreply(vaultConfig *vault.Config) (*EmailAccount, error) {
+	noreply := EmailAccount{}
+	err := vault.GetData(vaultConfig,
+		"/secret/data/accounts/noreplyemail", 0, &noreply)
+	if err != nil {
+		return nil, err
+	}
+	if noreply.SmtpPort == "" {
+		noreply.SmtpPort = "587"
+	}
+	return &noreply, nil
+}
