ceb5274d84e964374c014eae9fc8a863298a9994
Lev Shvarts <lev.shvarts@edgexr.net>
2023-01-19 11:26:12 -0800

    Changed the comparison for the undeploy rule to be strict with respect to active connections (#133)
    Help string for undeployclientcount states that the number has to be strictly less than the configured one - made the rule match the help string

--- pkg/autorules/autoprovrules.go
+++ pkg/autorules/autoprovrules.go
@@ -19,9 +19,9 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/edgexr/edge-cloud-platform/pkg/prommgmt"
-	"github.com/edgexr/edge-cloud-platform/pkg/cloudcommon"
 	"github.com/edgexr/edge-cloud-platform/api/edgeproto"
+	"github.com/edgexr/edge-cloud-platform/pkg/cloudcommon"
+	"github.com/edgexr/edge-cloud-platform/pkg/prommgmt"
 	"github.com/prometheus/common/model"
 )
 
@@ -37,7 +37,7 @@ func GetAutoUndeployRules(ctx context.Context, settings edgeproto.Settings, appK
 		edgeproto.AppKeyTagName + `="` + appKey.Name + `",` +
 		edgeproto.AppKeyTagVersion + `="` + appKey.Version + `",` +
 		edgeproto.AppKeyTagOrganization + `="` + appKey.Organization +
-		`"} <= ` + fmt.Sprintf("%d", policy.UndeployClientCount)
+		`"} < ` + fmt.Sprintf("%d", policy.UndeployClientCount)
 	forSec := int64(policy.UndeployIntervalCount) * int64(settings.AutoDeployIntervalSec)
 	rule.For = model.Duration(time.Second * time.Duration(forSec))
 	grp.Rules = append(grp.Rules, rule)
